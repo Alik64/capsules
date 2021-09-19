416427436e913b86f90d6c7a1a22b2e7beeaa513
Albert <albert.barsamov@gmail.com>
2021-09-19 16:14:49 +0200

    footer + responsive

--- Classic/classic.js
+++ Classic/classic.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector("#navbar");
 const burger = document.querySelector(".menu-mobile");
 
+// dès qu'on clique sur bouton de menu mobile, menu s'ouvre
 burger.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
