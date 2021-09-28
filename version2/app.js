9befd8aed0db76f5716db56c011eb69d249514f7
Albert <albert.barsamov@gmail.com>
2021-09-28 11:44:12 +0200

    modal demande

--- version2/app.js
+++ version2/app.js
@@ -1,1 +1,11 @@
+const demandeBtn = document.querySelector(".demandeIns");
+const formulaireDemandeIns = document.querySelector(".form-dem-inscription");
+const closeBtn = document.querySelector(".formCloseBtn");
 
+demandeBtn.addEventListener("click", () => {
+  formulaireDemandeIns.classList.add("open-modal");
+});
+
+closeBtn.addEventListener("click", () => {
+  formulaireDemandeIns.classList.remove("open-modal");
+});
