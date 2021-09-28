a4e8e9ae92104f67214cd44ea5b726fe9083c48d
Albert <albert.barsamov@gmail.com>
2021-09-28 11:47:20 +0200

    mise à jour du site

--- Classic/classic.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector("#navbar");
-const burger = document.querySelector(".menu-mobile");
-
-// dès qu'on clique sur bouton de menu mobile, menu s'ouvre
-burger.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
