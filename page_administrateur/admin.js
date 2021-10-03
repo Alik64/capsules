071cb8647459f5d3a75cd75d136fd2b8fe5a9472
Albert <albert.barsamov@gmail.com>
2021-10-04 01:09:17 +0200

    formulaire new CAps

--- page_administrateur/admin.js
+++ page_administrateur/admin.js
@@ -1,7 +1,24 @@
+const html = document.querySelector("html");
+
 // MENU Tablette + Portable open/close
 const menuMobil = document.querySelector(".menu-mobil");
 const headerMenu = document.querySelector(".header_menu");
 
 menuMobil.addEventListener("click", () => {
   headerMenu.classList.toggle("open");
+});
+
+// Ajout nouveau muselet
+
+const ajouterCaps = document.querySelector("#addNewCaps");
+const newCapsContainer = document.querySelector(".newCaps-container");
+const newCapsCloseBtn = document.querySelector(".newCapsCloseBtn");
+
+ajouterCaps.addEventListener("click", () => {
+  newCapsContainer.classList.add("openModal");
+  headerMenu.classList.remove("open");
+});
+
+newCapsCloseBtn.addEventListener("click", () => {
+  newCapsContainer.classList.remove("openModal");
 });
