ed995ee990e6c9ca839486a6b1c19bc4fcdf43e5
Albert <albert.barsamov@gmail.com>
2021-10-12 19:49:07 +0200

    Fusion fichier CSS + fichiers JS

--- New/app.js
+++ New/app.js
@@ -1,17 +1,3 @@
-// page d'accueil
-
-const demandeBtn = document.querySelector(".demandeIns");
-const formulaireDemandeIns = document.querySelector(".form-dem-inscription");
-const closeBtn = document.querySelector(".formCloseBtn");
-
-demandeBtn.addEventListener("click", () => {
-  formulaireDemandeIns.classList.add("open-modal");
-});
-
-closeBtn.addEventListener("click", () => {
-  formulaireDemandeIns.classList.remove("open-modal");
-});
-
 // Page ADMIN - Utilisateur
 
 // MENU Tablette + Portable open/close
