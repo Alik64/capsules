413ca188d5875d6c2e1a7714ebab54f27c48a6dd
Albert <albert.barsamov@gmail.com>
2021-10-14 10:44:10 +0200

    test

--- New/app.js
+++ New/app.js
@@ -18,7 +18,6 @@ rvButtons.forEach((button) => {
     const cardImg =
       e.target.parentElement.parentElement.parentElement.parentElement
         .parentElement.children[0].children[0];
-    console.log(cardImg);
 
     if (icoRV.classList.contains("verso")) {
       cardImg.src = cardImg.dataset.recto;
@@ -31,28 +30,3 @@ rvButtons.forEach((button) => {
     }
   });
 });
-
-// PAGE AJOUT / MODIFICATION - Capsule
-
-const inputRecto = document.querySelector("#newCaps-Recto");
-const inputVerso = document.querySelector("#newCaps-Verso");
-const imgRecto = document.querySelector("#imgRecto");
-const imgVerso = document.querySelector("#imgVerso");
-
-// Aperçu de l'image Recto
-inputRecto.onchange = (e) => {
-  const [file] = inputRecto.files;
-  if (file) {
-    imgRecto.src = URL.createObjectURL(file);
-    imgRecto.style.visibility = "visible";
-  }
-};
-// Aperçu de l'image Verso
-
-inputVerso.onchange = (e) => {
-  const [file] = inputVerso.files;
-  if (file) {
-    imgVerso.src = URL.createObjectURL(file);
-    imgVerso.style.visibility = "visible";
-  }
-};
